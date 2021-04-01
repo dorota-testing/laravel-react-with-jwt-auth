4046a9c68502888990bc2c30d7647e6714da0f9b
Dorota <d.a.lewinska@gmail.com>
2021-04-01 15:52:29 +0100

    React router was installed. Basic navigation to empty components was set up

--- resources/js/react/App.js
+++ resources/js/react/App.js
@@ -1,17 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 import Header from './components/Header/Header.js';
 import Example from './components/Example/Example.js';
+import About from './components/About/About.js';
+import Login from './components/Login/Login.js';
+import Register from './components/Register/Register.js';
 
 function App(props) {
     const laravelGlobals = JSON.parse(props.laravelGlobals);
     return (
-        <div>
-            <Header laravelGlobals={laravelGlobals}/>
-            <main className="py-4">
-                <Example />
-            </main>
-        </div>
+        <Router>
+            <div>
+                <Header laravelGlobals={laravelGlobals} />
+            </div>
+            <Switch>
+                <Route exact path="/">
+                    <main className="py-4">
+                        <Example />
+                    </main>
+                </Route>
+                <Route path="/about">
+                    <main className="py-4">
+                        <About />
+                    </main>
+                </Route>
+                <Route path="/login">
+                    <main className="py-4">
+                        <Login />
+                    </main>
+                </Route>
+                <Route path="/register">
+                    <main className="py-4">
+                        <Register />
+                    </main>
+                </Route>                               
+            </Switch>
+        </Router>
     );
 }
 
--- resources/js/react/components/Header/Header.js
+++ resources/js/react/components/Header/Header.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import './Header.css';
-
+import { Link, NavLink } from 'react-router-dom';
 
 function Header(props) {
   //console.log('laravelGlobals =', props);
   return (
     <nav className="navbar navbar-expand-md navbar-light bg-white shadow-sm">
       <div className="container">
-        <a className="navbar-brand" href= {props.laravelGlobals.baseUrl}>
-          {props.laravelGlobals.appName} 
-                </a>
+        <Link className="navbar-brand" to="/">{props.laravelGlobals.appName} </Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
+            <NavLink to="/about" activeClassName="active">About</NavLink>
           </ul>
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
-              <a className="nav-link" href= {props.laravelGlobals.baseUrl + '/dashboard'} >Login</a>
+              <NavLink className="nav-link" to="/login" activeClassName="active">Login</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/register" activeClassName="active">Register</NavLink>
             </li>
           </ul>
         </div>
